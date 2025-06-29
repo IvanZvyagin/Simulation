3ca62c671a3e39c3984959335898c4771fe7ad9b
IvanZvyagin <ivan.zvyagin20@gmail.com>
2025-06-29 22:52:22 +0300

    небольшие правки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import WorldMap.Simulation;
+
 public class Main {
     public static void main(String[] args) {
+        Simulation simulation = new Simulation(10,10);
 
     }
 }
--- src/WorldMap/Simulation.java
+++ src/WorldMap/Simulation.java
@@ -6,4 +6,7 @@ public class Simulation {
     private static final String COMMAND_START = "Start";
     private static final String COMMAND_PAUSE = "Pause";
     private boolean pause = false;
+    public Simulation(int lengthX,int widthY){
+        this.map = new WorldMap(lengthX,widthY);
+    }
 }
