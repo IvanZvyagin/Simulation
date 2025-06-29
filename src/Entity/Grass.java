4252a743ddd331817bc369eaaadddd6209161d03
IvanZvyagin <ivan.zvyagin20@gmail.com>
2025-06-29 16:12:10 +0300

    Обновлен класс Grass

--- src/Entity/Grass.java
+++ src/Entity/Grass.java
@@ -2,4 +2,18 @@ package Entity;
 
 
 public class Grass extends Entity {
+    private int hp;
+
+    public Grass(String sprite, int hp) {
+        super(sprite);
+        this.hp = hp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
