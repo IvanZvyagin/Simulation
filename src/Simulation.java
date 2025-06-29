55c97f9003b94300486ffd47b24fd56c9773a3bf
IvanZvyagin <ivan.zvyagin20@gmail.com>
2025-06-29 22:50:14 +0300

    Добавлен класс Simulation, внесены поля(пока не все)

--- src/Simulation.java
+++
@@ -1,9 +0,0 @@
-import Creature.Predator;
-import Entity.EntityType;
-import WorldMap.WorldMap;
-
-public class Simulation {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/WorldMap/Simulation.java
@@ -0,0 +1,9 @@
+package WorldMap;
+
+public class Simulation {
+    private WorldMap map;
+    private int moveCounter = 0;
+    private static final String COMMAND_START = "Start";
+    private static final String COMMAND_PAUSE = "Pause";
+    private boolean pause = false;
+}
